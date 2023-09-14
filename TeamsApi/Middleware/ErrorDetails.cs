a0000fc8d4cae11204746a5fa838ef09913444c8
hacksJuanDavid <josuacoty000@gmail.com>
2023-09-14 02:48:20 -0500

    Format ErrorDetails and ExceptionMiddleware

--- TeamsApi/Middleware/ErrorDetails.cs
+++ TeamsApi/Middleware/ErrorDetails.cs
@@ -3,11 +3,11 @@ using Newtonsoft.Json;
 namespace TeamsApi.Middleware;
 
 public class ErrorDetails
-{   
+{
     public string? ErrorType { get; set; }
-    
+
     public List<string>? Errors { get; set; }
-    
+
     public override string ToString()
     {
         return JsonConvert.SerializeObject(this);
--- TeamsApi/Middleware/ExceptionMiddleware.cs
+++ TeamsApi/Middleware/ExceptionMiddleware.cs
@@ -3,19 +3,19 @@ namespace TeamsApi.Middleware;
 public class ExceptionMiddleware
 {
     private readonly RequestDelegate _next;
-    
+
     public ExceptionMiddleware(RequestDelegate next)
     {
         _next = next ?? throw new ArgumentNullException(nameof(next));
     }
-    
+
     public async Task InvokeAsync(HttpContext httpContext)
     {
         if (httpContext is null)
         {
             throw new ArgumentNullException(nameof(httpContext));
         }
-        
+
         try
         {
             await _next(httpContext);
